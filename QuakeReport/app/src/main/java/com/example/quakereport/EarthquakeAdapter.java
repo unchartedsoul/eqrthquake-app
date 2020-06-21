f3d678b48dc32fae854d9c2f0a6f62d7bb83917c
unchartedsoul <sudanshu144@gmail.com>
2020-06-21 18:13:29 +0530

    Update EarthquakeAdapter.java

--- QuakeReport/app/src/main/java/com/example/quakereport/EarthquakeAdapter.java
+++ QuakeReport/app/src/main/java/com/example/quakereport/EarthquakeAdapter.java
@@ -18,25 +18,43 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake>{
         super(context,0, earthquakes);
     }
 
+    /**
+     * Returna a list item taht displays information about the earthquak at the time of earthquake
+     * in the list of earthquake.
+     */
+
+
+    @Override
     public View getView(int postion, View convertView,ViewGroup parent){
+        // Check if there is an existing list item view (called convertView) that we can resume,
+        //otherwise, if convert view is null, then inflate a new list item layout.
         View listItemView = convertView;
         if ( listItemView== null){
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.earthquake_list_items,parent,false);
         }
+
+        //find the earthquake at the given position in the list of earthquake.
         Earthquake currentEarthquake = getItem(postion);
 
+        // Find the textView with view ID magnitude.
         TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
+        // Display the magnitude of the current earthquake in that textView.
         magnitudeView.setText(currentEarthquake.getMagnitude());
 
 
+        // Find the textView with view ID location.
         TextView locationView = (TextView)listItemView.findViewById(R.id.location);
+        // Display the location of the current earthquake in that textView.
         locationView.setText(currentEarthquake.getLocation());
-        
-        
+
+
+        // Find the textView with view ID date.
         TextView dateView = (TextView)listItemView.findViewById(R.id.date);
+        // Display the date of the current earthquake in that textView.
         dateView.setText(currentEarthquake.getDate());
         
+        //Return the listView item that is shopwing the appropriate data.
         return listItemView;
     }
 }
